71f95c09e162b791eecc967f071d024029bf4f8b
Jaruwan <jaruwanspy1997@gmail.com>
2017-01-04 19:53:26 +0700

    Initial commit

---
+++ app/src/main/java/rtc/thanyalak/jaruwan/gamebetting/Money.java
@@ -0,0 +1,20 @@
+package rtc.thanyalak.jaruwan.gamebetting;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Money extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_money);
+
+
+    }
+}
